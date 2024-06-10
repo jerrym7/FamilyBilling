9665b649b42897de0f9a32dfdb828e0d1abf7dde
Gerardo Martinez <diino4l@gmail.com>
2024-06-09 21:11:21 -0700

    bad calendar

--- src/screens/HistoryScreen.jsx
+++ src/screens/HistoryScreen.jsx
@@ -1,12 +1,26 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {Dimensions, SafeAreaView, StyleSheet, Text, View} from 'react-native';
 import React from 'react';
+import {CalendarList} from 'react-native-calendars';
 
 export default function HistoryScreen() {
   return (
-    <View>
-      <Text>Billing list</Text>
-    </View>
+    <SafeAreaView>
+      <CalendarList
+        // Enable horizontal scrolling, default = false
+        horizontal={true}
+        // Enable paging on horizontal, default = false
+        pagingEnabled={true}
+        // Set custom calendarWidth.
+        calendarWidth={styles.screenStyle.width}
+        calendarHeight={styles.screenStyle.width}
+      />
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  screenStyle: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  },
+});
